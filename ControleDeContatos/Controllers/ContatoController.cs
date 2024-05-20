5fbd314cb6bea9a640a6a2c06a7d89cd68485d40
Ana Maria <Ana Maria@DESKTOP-VVV9T97>
2024-05-19 22:15:01 -0300

    UPDATE: função do apagar atualizada

--- ControleDeContatos/Controllers/ContatoController.cs
+++ ControleDeContatos/Controllers/ContatoController.cs
@@ -34,37 +34,63 @@ namespace ControleDeContatos.Controllers
                 await _context.SaveChangesAsync();
                 return RedirectToAction("Index");
             }
-            throw new Exception("Preencha todos os campos!");
-            
-        }
-        public IActionResult Editar()
-        {
             return View();
         }
+        public IActionResult Editar(int id)
+        {
+            var contato = _context.Contato.FirstOrDefault(x => x.id == id);
+            if (contato == null)
+            {
+                return NotFound();
+            }
+            return View(contato);
+        }
+
         [HttpPost]
         public async Task<IActionResult> Editar(ContatoModels contato)
         {
-            if (contato != null)
+            if (ModelState.IsValid)
             {
-                var busca = _context.Contato.FirstOrDefault(x => x.id == contato.id);
-
+                var busca = await _context.Contato.FirstOrDefaultAsync(x => x.id == contato.id);
                 if (busca == null)
                 {
-                    throw new Exception("Dados não encontrados!");
+                    return NotFound();
                 }
+
                 busca.Nome = contato.Nome;
                 busca.Email = contato.Email;
                 busca.Celular = contato.Celular;
 
-                await _context.Contato.AddAsync(busca);
+                _context.Update(busca);
                 await _context.SaveChangesAsync();
                 return RedirectToAction("Index");
             }
-            return View();
+            return View(contato);
         }
-        public IActionResult ApagarConfirmacao()
+        public IActionResult ApagarConfirmacao(int id)
         {
-            return View();
+            var contato = _context.Contato.FirstOrDefault(x => x.id == id);
+            if (contato == null)
+            {
+                return NotFound();
+            }
+            return View(contato);
+        }
+
+        public async Task<IActionResult> Apagar(int id)
+        {
+            ContatoModels contato = _context.Contato.FirstOrDefault(x => x.id == id);
+
+            if (contato == null)
+            {
+                throw new System.NotImplementedException("Houve um erro na deleção do contato!");
+            }
+            _context.Contato.Remove(contato);
+            _context.SaveChanges();
+
+
+            return RedirectToAction("Index");
+            
         }
     }
 }
--- ControleDeContatos/Models/ContatoModels.cs
+++ ControleDeContatos/Models/ContatoModels.cs
@@ -1,12 +1,19 @@
-﻿namespace ControleDeContatos.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ControleDeContatos.Models
 {
     public class ContatoModels
     {
         public int id { get; set; }
+        [Required(ErrorMessage ="Digite o nome do contato!")]
 
         public string Nome { get; set; }
+        [Required(ErrorMessage = "Digite o email do contato!")]
+        [EmailAddress(ErrorMessage ="O email digitado não é válido!")]
 
         public string Email { get; set; }
+        [Required(ErrorMessage = "Digite o celular do contato!")]
+        [Phone(ErrorMessage ="O celular digitado não é válido")]
 
         public string Celular { get; set;}
     }
